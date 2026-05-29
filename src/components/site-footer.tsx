3c47373c60d46e60091e74255e183158f1c6ce18
Jacob Liberty <jacob.tobin.liberty@gmail.com>
2026-05-29 11:49:43 -0400

    feat: add footer and themed home splash page

---
+++ src/components/site-footer.tsx
@@ -0,0 +1,7 @@
+export function SiteFooter() {
+  return (
+    <footer className="mt-16 border-t border-terracotta/50 py-8 text-center text-cocoa/70">
+      <p className="font-hand text-lg">made with 🐾 by Jacob &amp; Lily</p>
+    </footer>
+  );
+}
