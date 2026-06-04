c4cc7378d7c33e7c22cae0dd4fc328c0a4132354
Jacob Liberty <jacob.tobin.liberty@gmail.com>
2026-06-04 17:29:59 -0400

    feat: plan query + types

---
+++ src/sanity/lib/plan-queries.ts
@@ -0,0 +1,24 @@
+import { defineQuery } from "next-sanity";
+
+export const PLAN_QUERY = defineQuery(`
+  *[_id == "mealPlan"][0]{
+    "recipes": recipes[]->{
+      _id,
+      title,
+      "slug": slug.current,
+      "coverImage": images[0],
+      "ingredients": ingredients[]{
+        "ingredientId": ingredient._ref,
+        "name": ingredient->name,
+        quantity,
+        unit
+      }
+    },
+    manualItems[]{ _key, name, gotIt },
+    checkedIngredients
+  }
+`);
+
+export const PLAN_RECIPE_IDS_QUERY = defineQuery(`
+  *[_id == "mealPlan"][0].recipes[]._ref
+`);
---
+++ src/sanity/plan-types.ts
@@ -0,0 +1,18 @@
+import type { SanityImageSource } from "@sanity/image-url";
+import type { PlanIngredient } from "@/lib/grocery";
+
+export type PlanRecipe = {
+  _id: string;
+  title: string;
+  slug: string;
+  coverImage?: SanityImageSource | null;
+  ingredients: PlanIngredient[] | null;
+};
+
+export type ManualItem = { _key: string; name: string; gotIt: boolean };
+
+export type PlanData = {
+  recipes: PlanRecipe[] | null;
+  manualItems: ManualItem[] | null;
+  checkedIngredients: string[] | null;
+};
